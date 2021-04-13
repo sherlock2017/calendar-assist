8959fbd6c7db5c102eb721ecf34ba68ca6ea1a05
Rishabh Jain <rishabh.jain2@fisglobal.com>
2021-04-13 19:04:36 +0530

    Update URI and using pathparam instead of pathvariable

--- src/main/java/com/calendar/assist/controller/CalendarController.java
+++ src/main/java/com/calendar/assist/controller/CalendarController.java
@@ -4,6 +4,8 @@ import java.math.BigInteger;
 import java.time.LocalDate;
 import java.util.Set;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,10 +42,10 @@ public class CalendarController {
 	 * @param durationInMins
 	 * @return
 	 */
-	@GetMapping("free-slots/{emp1}/{emp2}/{calendarDate}/{durationInMins}")
-	public Set<TimeSlotDto> getAvailableSlots(@PathVariable("emp1") final BigInteger emp1,
-			@PathVariable("emp2") final BigInteger emp2, @PathVariable("calendarDate") @DateTimeFormat(pattern = "yyyy-MM-dd") final LocalDate calendarDate,
-			@PathVariable("durationInMins") final int durationInMins) {
-		return calendarService.getAvailableSlots(emp1, emp2, calendarDate, durationInMins);
+	@GetMapping("available-slots")
+	public Set<TimeSlotDto> getAvailableSlots(@PathParam("empId1") final BigInteger empId1,
+			@PathParam("empId2") final BigInteger empId2, @PathParam("calendarDate") @DateTimeFormat(pattern = "yyyy-MM-dd") final LocalDate calendarDate,
+			@PathParam("durationInMins") final int durationInMins) {
+		return calendarService.getAvailableSlots(empId1, empId2, calendarDate, durationInMins);
 	}
 }
